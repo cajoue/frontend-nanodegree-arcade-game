c7a5d331f1c7644bab1161b1f6fb3b2606107fe0
cajoue <cajoue@users.noreply.github.com>
2015-11-13 14:32:56 +0100

    feat: create Player() instantiate allEnemies[] and player

--- js/app.js
+++ js/app.js
@@ -24,12 +24,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress){
+    // tbd
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
